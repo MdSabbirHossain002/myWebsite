c1173d1dea20312be32258584f04cb63250c059e
Md Sabbir Hossain <sabbirhossain002@outlook.com>
2022-05-15 12:23:05 +0600

    15-may-22

---
+++ main.js
@@ -0,0 +1,30 @@
+var gForm = document.querySelector(".g-form");
+var left = document.querySelector(".left");
+var bars = document.querySelector(".bars");
+var barImg = document.querySelector(".bars img");
+var layer = document.querySelector(".layer");
+var utility = document.querySelector(".utility");
+var utilityItems = document.querySelector(".utility-items");
+// if(window.width < 500px){
+ 
+
+// }
+   
+bars.addEventListener('click', function () {
+  left.style.left = '80%';
+  layer.style.width='100%';
+  layer.style.background ='gray';
+  layer.style.opacity = '.4';
+  bars.style.display ='none';
+});
+layer.addEventListener('click', function(){
+    left.style.left = '0%';
+    layer.style.width='0';
+    bars.style.display= 'inline';
+});
+
+utility.addEventListener('click',()=>{
+  utilityItems.classList.toggle("collapse");
+  utilityItems.classList.toggle("uncollapse");
+
+})
